0b2d02716cc5c997636b5e5d768bd16c818cf7d3
Churchill perry <churchill.perry@bryanuniversity.edu>
2022-03-28 01:22:57 -0500

    In and Out With Python

---
+++ week1/apples.py
@@ -0,0 +1,4 @@
+applePrice = 0.50
+name = input('Please enter your name: ')
+number = input(f'Hi {name}. Apples cost ${applePrice:.2f} each. How many would you like to buy? ')
+print(f'Thank you {name} for your purchase of {number} apples at a cost of ${applePrice:.2f} each.')
