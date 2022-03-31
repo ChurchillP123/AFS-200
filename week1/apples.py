334b78a618c2a9c1ff64c6ea63b41ab960b7068f
Churchill perry <churchill.perry@bryanuniversity.edu>
2022-03-31 02:00:21 -0500

    convert str to int

--- week1/apples.py
+++ week1/apples.py
@@ -1,4 +1,7 @@
 applePrice = 0.50
 name = input('Please enter your name: ')
-number = input(f'Hi {name}. Apples cost ${applePrice:.2f} each. How many would you like to buy? ')
+#input from terminal returns a string so result must be casted to an integer
+number = int(input(f'Hi {name}. Apples cost ${applePrice:.2f} each. How many would you like to buy? '))
 print(f'Thank you {name} for your purchase of {number} apples at a cost of ${applePrice:.2f} each.')
+#this print statement is to check that number is indeed an integer and not a string 
+print(type(number))
