909731d5e2bbc1124ee9f886b0d1fcf4a2204605
Churchill perry <churchill.perry@bryanuniversity.edu>
2022-04-10 23:45:33 -0500

    guess the secret word

--- week3/guess.py
+++ week3/guess.py
@@ -1,4 +1,4 @@
-secretWord = 'justice'
+secretWord = 'professor'
 guessLetters = []
 num_of_guesses = 0
 
@@ -7,37 +7,49 @@ for letter in range(len(secretWord)):
     wordBoard.append('_')
 
 def showBoard():
-    print(wordBoard)
+    print(''.join(wordBoard))
 
 def checkGuess(guessL):
+    guessL = guessL.lower()
     index = secretWord.find(guessL)
-   
-    if(index.isdigit() and index != -1):
+    
+    if(index > -1):
         indices = [i for i in range(len(secretWord)) if secretWord[i] == guessL]
         for i in indices:
-            wordBoard.replace(i, guessL)
+            wordBoard[i] = guessL
+        if (secretWord.count(guessL) > 1):
+            print(f"Yes there are {secretWord.count(guessL)} {guessL} ")
+        else:
+            print(f"Yes there is a {guessL}")
         guessLetters.append(guessL)
         return True
     else:
+        print(f"Im sorry but there is no letter {guessL} in the word")
+        guessLetters.append(guessL)
         return False
 
-print(wordBoard)
+def split(word):
+    return [char for char in word]
 
 def guessingGame():
-    guessLetter = input("Guess a Letter: ")
-    while(wordBoard.index(guessLetter) > 1):
-        guessLetter = input("Guess Another Letter: ")
-    if (checkGuess(guessLetter)):
-        showBoard()
-    else:
-        num_of_guesses += 1
-        if(num_of_guesses > 5):
-            print(f"Game Over! The secret word was...{secretWord}")
+    print("Can you guess the secret password?")
+    showBoard()
+    global num_of_guesses
+    while(num_of_guesses < 5):
+        guessLetter = input("Guess a Letter: ").lower()
+        
+        while(guessLetters.count(guessLetter) > 0):
+            guessLetter = input("Guess Another Letter: ")
+        if (checkGuess(guessLetter)):
+            showBoard()
+            if (split(secretWord) == wordBoard):
+                print("Congratulations, You Won!")
+                break
+        else:
+            num_of_guesses += 1
+            print(f"You have {5 - num_of_guesses} chances left")
+            showBoard()
+            if(num_of_guesses > 4):
+                print(f"Game Over! The secret word was...{secretWord}")
 
-
-
-
-
-
-
-
+guessingGame()
--- week3/onlyeven.py
+++ week3/onlyeven.py
@@ -3,7 +3,6 @@ def prompt(isValid = True):
         return input("Enter a positive number: ")
     return input("Invalid Entry. Please enter a positive number: ")
     
-
 def countDown(num):
     for num in range(0, int(num)+1, 2):
         print(num)
